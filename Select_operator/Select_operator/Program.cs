b61255b5b6cdd5ecefc06c0701e34dba922dd111
theavaniparmar <avniparmar515@gmail.com>
2025-03-07 17:54:09 +0530

    select data

---
+++ Select_operator/Select_operator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDemo
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> list = new List<Employee>
+            {
+                new Employee { Id = 1, Name = "abc"},
+                new Employee { Id = 2, Name = "xyz"}
+            };
+            return list;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //method syntax
+            IEnumerable<Employee> method = Employee.GetEmployees().ToList();
+            foreach (Employee emp in method)
+            {
+                Console.WriteLine($"Id : {emp.Id}  Name : {emp.Name}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
