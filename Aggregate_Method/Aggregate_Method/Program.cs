48f6391e0a3e4611d3c001a88f0491b277692df9
theavaniparmar <avniparmar515@gmail.com>
2025-03-10 16:04:47 +0530

    new file added

---
+++ Aggregate_Method/Aggregate_Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+
+            int result = arr.Aggregate((acc, num) => acc * num);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
