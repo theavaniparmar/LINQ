5fcc4ce765a69e69a4b117f150f07f7b6ab8bc9c
theavaniparmar <avniparmar515@gmail.com>
2025-03-10 12:39:43 +0530

    use select many operator

---
+++ SelectMany_operator/SelectMany_operator/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectMany_operator
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+            
+        public string Email { get; set; }
+        public List<string> Programming { get; set; }
+
+        public static List<Class1> Get()
+        { return new List<Class1>()
+            {
+                new Class1 (){Id = 1, Name = "James", Email = "James@j.com", Programming = new List<string>() { "C#", "Jave", "C++"} },
+                new Class1 (){Id = 2, Name = "Sam", Email = "Sara@j.com", Programming = new List<string>() { "WCF", "SQL Server", "C#" }},
+                new Class1(){Id = 3, Name = "Patrik", Email = "Patrik@j.com", Programming = new List<string>() { "MVC", "Jave", "LINQ"} },
+                new Class1(){Id = 4, Name = "Sara", Email = "Sara@j.com", Programming = new List<string>() { "ADO.NET", "C#", "LINQ" } }
+            }; 
+        }
+    }
+}
---
+++ SelectMany_operator/SelectMany_operator/Program.cs
@@ -0,0 +1,27 @@
+﻿using SelectMany_operator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // using method syntax
+            var method = Class1.Get().SelectMany(c => c.Programming, (student, program) => new
+            {
+                StudentName = student.Name,
+                ProgrammingSkills = program
+            }).ToList();
+
+            foreach(var s in method)
+            {
+                Console.WriteLine(s.StudentName + "=>" + s.ProgrammingSkills);
+            }
+            Console.ReadKey();
+        }    
+    }
+}
+ 
